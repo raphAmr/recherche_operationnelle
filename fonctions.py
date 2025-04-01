d24294ec5fa523bba6e343a22332ce24e7ff3dab
raphAmr <raphamour@gmail.com>
2025-04-01 13:41:42 +0200

    init fichier

---
+++ fonctions.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+#input
+#selectionne le fichier et le retranscrit dans une variable
+#output : la variable contenant la matrice
+def lecture(fichier):
+    pass
---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import fonctions as fct
+
+
+print ("-----------------Bienvenu sur le projet de recherche opérationnelle.---------------------------------")
+
+running = True
+
+while running:
+    fichier = -1
+    while not(0 <= fichier and fichier <= 20):
+        fichier = int(input("Veuillez entre le numéro de fichier à travailler : "))
+    if fichier == 0:
+        running = False
+    else:
+        matrice = fct.lecture(fichier)
+    
